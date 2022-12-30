ac9d7d4b3cedd1095e20d4204d1e0d53262de3fd
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2022-12-30 15:05:20 +0100

    display 5 questions on index view and raise expception on detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -9,11 +9,18 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list,
+    }
     return HttpResponse(output)
 
 #Question “detail” page – displays a question text, with no results but with a form to vote
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 #Question “results” page – displays results for a particular question.
 def results(request, question_id):
     response = "You're looking at the results of question %s."
