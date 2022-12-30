dc5b29c028a89eb3a07adc923331307812b723ad
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2022-12-30 16:01:16 +0100

    Removing hardcoded URLs in templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 #Question “detail” page – displays a question text, with no results but with a form to vote
 def detail(request, question_id):
